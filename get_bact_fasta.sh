af57a577fe0d35c43d13b241eb3f056668d28384
tsvvas <vasily.o.tsvetkov@gmail.com>
2019-10-15 21:21:03 +0300

    Added data and utils

--- get_bact_fasta.sh
+++ get_bact_fasta.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-wget -O "./Chlamydia&trachomatis.fasta" "http://mbgd.genome.ad.jp/htbin/getGeneSequence.pl?sp=ctr&format=fasta"
-wget -O "./Yersinia&enterocolitica.fasta" "http://mbgd.genome.ad.jp/htbin/getGeneSequence.pl?sp=gm04749&format=fasta"
-wget -O "./Yersinia&pseudotuberculosis.fasta" "http://mbgd.genome.ad.jp/htbin/getGeneSequence.pl?sp=gm04772&format=fasta"
-wget -O "./Salmonella&enteritidis.fasta" "http://mbgd.genome.ad.jp/htbin/getGeneSequence.pl?sp=gm04530&format=fasta"
-wget -O "./Salmonella&typhimurium.fasta" "http://mbgd.genome.ad.jp/htbin/getGeneSequence.pl?sp=stm&format=fasta"
-wget -O "./Shigella&flexneri.fasta" "http://mbgd.genome.ad.jp/htbin/getGeneSequence.pl?sp=sfl&format=fasta"
-wget -O "./Shigella&sonnei.fasta" "http://mbgd.genome.ad.jp/htbin/getGeneSequence.pl?sp=ssj&format=fasta"
-wget -O "./Campylobacter&jejuni.fasta" "http://mbgd.genome.ad.jp/htbin/getGeneSequence.pl?sp=cje&format=fasta"
-wget -O "./Clostridium&difficile.fasta" "http://mbgd.genome.ad.jp/htbin/getGeneSequence.pl?sp=cdf&format=fasta"
-wget -O "./Mycoplasma&pneumoniae.fasta" "http://mbgd.genome.ad.jp/htbin/getGeneSequence.pl?sp=mpj&format=fasta"
-wget -O "./Mycobacterium&smegmatis.fasta" "http://mbgd.genome.ad.jp/htbin/getGeneSequence.pl?sp=msm&format=fasta"
-wget -O "./Klebsiella&pneumoniae.fasta" "http://mbgd.genome.ad.jp/htbin/getGeneSequence.pl?sp=kpn&format=fasta"
-wget -O "./Mycoplasma&synoviae.fasta" "http://mbgd.genome.ad.jp/htbin/getGeneSequence.pl?sp=msy&format=fasta"
+wget -O "CMV_AD169.fasta" "https://www.uniprot.org/uniprot/?include=false&format=fasta&force=true&query=proteome:UP000008991"
+wget -O "EBV_AG876.fasta" "https://www.uniprot.org/uniprot/?include=false&format=fasta&force=true&query=proteome:UP000007639"
+wget -O "HIV1_HXB2.fasta" "https://www.uniprot.org/uniprot/?include=false&format=fasta&force=true&query=proteome:UP000002241"
+wget -O "YFV_17D.fasta" "https://www.uniprot.org/uniprot/?include=false&format=fasta&force=true&query=proteome:UP000000360"
+wget -O "HCV_IsoH.fasta" "https://www.uniprot.org/uniprot/?include=false&format=fasta&force=true&query=proteome:UP000000518"
+wget -O "CLMD_trachomatis.fasta" "https://www.uniprot.org/uniprot/?include=false&format=fasta&force=true&query=proteome:UP000000431"
+wget -O "YERS_pestis.fasta" "https://www.uniprot.org/uniprot/?include=false&format=fasta&force=true&query=proteome:UP000000815"
+wget -O "SLML_typhimurium.fasta" "https://www.uniprot.org/uniprot/?include=false&format=fasta&force=true&query=proteome:UP000001014"
+wget -O "SHGL_flexneri.fasta" "https://www.uniprot.org/uniprot/?include=false&format=fasta&force=true&query=proteome:UP000001006"
+wget -O "CPBT_jejuni.fasta" "https://www.uniprot.org/uniprot/?include=false&format=fasta&force=true&query=proteome:UP000000799"
+wget -O "CLOS_difficile.fasta" "https://www.uniprot.org/uniprot/?include=false&format=fasta&force=true&query=proteome:UP000001978"
+wget -O "MYPL_pneumoniae.fasta" "https://www.uniprot.org/uniprot/?include=false&format=fasta&force=true&query=proteome:UP000000808"
+wget -O "MYBT_smegmatis.fasta" "https://www.uniprot.org/uniprot/?include=false&format=fasta&force=true&query=proteome:UP000000757"
+wget -O "KLEB_pneumoniae.fasta" "https://www.uniprot.org/uniprot/?include=false&format=fasta&force=true&query=proteome:UP000000265"
+wget -O "MYPL_synoviae.fasta" "https://www.uniprot.org/uniprot/?include=false&format=fasta&force=true&query=proteome:UP000000549"
+wget -O "human_proteome.fasta" "https://www.uniprot.org/uniprot/?include=false&format=fasta&force=true&query=proteome:UP000005640"
+
 mkdir -p data/fasta/
-cp *fasta data/fasta/
+mv *fasta data/fasta/
